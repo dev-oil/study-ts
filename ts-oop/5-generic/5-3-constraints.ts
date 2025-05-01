86e9b4d7abc450cbd1855a5fa7c669ee199c780c
devoil <devdevoildev@gmail.com>
2025-05-01 22:18:45 +0900

    docs: [ts-oop] 5. generic의 제약조건 - 공변성 / 반공변성 / 불변성

--- ts-oop/5-generic/5-3-constraints.ts
+++ ts-oop/5-generic/5-3-constraints.ts
@@ -3,6 +3,41 @@ interface Employee {
   pay(): void;
 }
 
+// FullTimeEmployee < Employee
+// const x: FullTimeEmployee = {}
+
+function welcomeFulltimeEmployee(employee: Employee): FullTimeEmployee {
+  // employee.workFullTime();
+  console.log(`Welcome to the full time employee!!`);
+  return new FullTimeEmployee();
+}
+// welcomeFulltimeEmployee :: Employee -> FullTimeEmployee
+
+// covariance, invariance
+const f: (employee: FullTimeEmployee) => Employee = welcomeFulltimeEmployee;
+
+// 입구를 좁게 만드는거 ㅇㅋ 넓히면 안돼
+// 출구를 넓게 만드는거 ㅇㅋ 좁히면 안돼
+
+// 1. Covariance (공변성)
+// 출구(반환값)에 적용됨
+// 반환 타입이 더 구체적(하위 타입)이어도 괜찮음
+
+// 2. Contravariance (반공변성)
+// 입구(매개변수)에 적용됨
+// 매개변수 타입이 더 넓은(상위 타입)이어도 괜찮음
+
+// 3. Invariance (불변성)
+// 정확히 일치해야 함
+// 서로 치환 불가
+
+// ⁉️ 하여튼 결론은
+// 입구는 넓게 받아야 안전하고, 출구는 좁게 내보내야 안전하다. (입구는 넓게, 출구는 좁게 → 타입 안전)
+
+// function createEmployee(): Employee {
+//   return new Employee()
+// }
+
 class FullTimeEmployee implements Employee {
   pay() {
     console.log(`full time!!`);
@@ -23,6 +58,7 @@ function payBad(employee: Employee): Employee {
   return employee;
 }
 
+// X도민
 function pay<T extends Employee>(employee: T): T {
   employee.pay();
   return employee;
